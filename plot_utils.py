ab229bdbf83c32a384acefdaf6bdade003ddd88f
Marius <mariuspotgieter@gmail.com>
2018-09-25 17:53:52 +0200

    Fix warning for matplot

--- plot_utils.py
+++ plot_utils.py
@@ -12,6 +12,6 @@ def plot_values(V):
 	im = ax.imshow(V, cmap='cool')
 	for (j,i),label in np.ndenumerate(V):
 	    ax.text(i, j, np.round(label,3), ha='center', va='center', fontsize=14)
-	plt.tick_params(bottom='off', left='off', labelbottom='off', labelleft='off')
+	plt.tick_params(bottom=False, left=False, labelbottom=False, labelleft=False)
 	plt.title('State-Value Function')
 	plt.show()
